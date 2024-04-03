aff3c8c7e2a01a7435059c6f03e4d01341f148b6
silindaM <silindanemdu@gmail.com>
2024-04-03 22:41:31 +0200

    setup frontend

---
+++ templateui/src/pages/dashboard/DashboardPaged.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardPaged = () => {
+  return (
+    <div>DashboardPaged</div>
+  )
+}
+
+export default DashboardPaged
---
+++ templateui/src/pages/dashboard/MyLogs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyLogs = () => {
+  return (
+    <div>MyLogs</div>
+  )
+}
+
+export default MyLogs
