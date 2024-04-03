ae8f254d3fbce15db5ab2f428bbbcefde7468d62
silindaM <silindanemdu@gmail.com>
2024-04-03 10:45:29 +0200

    configure identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,48 +1,85 @@
-
+using Domain.Account;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
-// 1. DbContext
-
+// Configure services
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(
-        configuration.GetConnectionString("DefaultConnection"),
-        sqlServerOptionsAction: sqlOptions =>
-        {
-            sqlOptions.EnableRetryOnFailure();
-        });
+  options.UseSqlServer(
+      configuration.GetConnectionString("DefaultConnection"),
+      sqlServerOptionsAction: sqlOptions =>
+      {
+        sqlOptions.EnableRetryOnFailure();
+      });
 });
-
 
 builder.Services.AddControllers()
-  .AddJsonOptions(options =>
-{
-  options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
+    .AddJsonOptions(options =>
+    {
+      options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 builder.Services.AddEndpointsApiExplorer();
-//dependency injection
-//identity
-//authentcation
+builder.Services.AddSwaggerGen();
+
+// Identity configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+  options.Password.RequiredLength = 8;
+  options.Password.RequireDigit = false;
+  options.SignIn.RequireConfirmedEmail = false;
+  options.SignIn.RequireConfirmedPhoneNumber = false;
+  options.SignIn.RequireConfirmedAccount = false;
+});
+
+// Authentication configuration
+builder.Services
+    .AddAuthentication(options =>
+    {
+      options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+      options.SaveToken = true;
+      options.RequireHttpsMetadata = false;
+      options.TokenValidationParameters = new TokenValidationParameters()
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        ValidAudience = configuration["JWT:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+      };
+    });
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-
 
 app.Run();
