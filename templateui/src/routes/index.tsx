b84bfc2d84d0aa68dd777a8497e5fc8407c05346
silindaM <silindanemdu@gmail.com>
2024-04-04 23:00:48 +0200

    fix minor issues

--- templateui/src/routes/index.tsx
+++ templateui/src/routes/index.tsx
@@ -22,46 +22,46 @@ import OwnerPage from '../pages/dashboard/OwnerPage'
 import NotFoundPage from '../pages/public/NotFoundPage'
 
 const GlobalRouter = () => {
-  return (
-    <Routes>
-        <Route element={<Layout/>}>
-        <Route index element={<HomePage/>}/>
-        <Route path={PATH_PUBLIC.register} element={<RegisterPage/>}/>
-        <Route path={PATH_PUBLIC.login} element={<LoginPage/>}/>
-        <Route path={PATH_PUBLIC.unauthorized} element={<UnauthorizedPage/>}/>
+    return (
+      <Routes>
+        {/* <Route path='' element /> */}
+        <Route element={<Layout />}>
+          
+          {/* Public routes */}
+          <Route index element={<HomePage />} />
+          <Route path={PATH_PUBLIC.register} element={<RegisterPage />} />
+          <Route path={PATH_PUBLIC.login} element={<LoginPage />} />
+          <Route path={PATH_PUBLIC.unauthorized} element={<UnauthorizedPage />} />
+  
+          {/* Protected routes -------------------------------------------------- */}
+          <Route element={<AuthGuard roles={allAccessRoles} />}>
+            <Route path={PATH_DASHBOARD.dashboard} element={<DashboardPage />} />
+            <Route path={PATH_DASHBOARD.sendMessage} element={<SendMessagePage />} />
+            <Route path={PATH_DASHBOARD.inbox} element={<InboxPage />} />
+            <Route path={PATH_DASHBOARD.myLogs} element={<MyLogsPage />} />
+            <Route path={PATH_DASHBOARD.user} element={<UserPage />} />
+          </Route>
+          <Route element={<AuthGuard roles={managerAccessRoles} />}>
+            <Route path={PATH_DASHBOARD.manager} element={<ManagerPage />} />
+          </Route>
+          <Route element={<AuthGuard roles={adminAccessRoles} />}>
+            <Route path={PATH_DASHBOARD.usersManagement} element={<UserManagementPage />} />
+            <Route path={PATH_DASHBOARD.updateRole} element={<UpdateRolePage />} />
+            <Route path={PATH_DASHBOARD.allMessages} element={<AllMessagesPage />} />
+            <Route path={PATH_DASHBOARD.systemLogs} element={<SystemLogsPage />} />
+            <Route path={PATH_DASHBOARD.admin} element={<AdminPage />} />
+          </Route>
+          <Route element={<AuthGuard roles={ownerAccessRoles} />}>
+            <Route path={PATH_DASHBOARD.owner} element={<OwnerPage />} />
+          </Route>
+          {/* Protected routes -------------------------------------------------- */}
+  
+          {/* Catch all (404) */}
+          <Route path={PATH_PUBLIC.notFound} element={<NotFoundPage />} />
+          <Route path='*' element={<Navigate to={PATH_PUBLIC.notFound} replace />} />
         </Route>
-        
-        {/*Protected Roles all users */}
-        <Route element={<AuthGuard roles={allAccessRoles}/>}>
-            <Route path={PATH_DASHBOARD.dashboard} element={<DashboardPage/>}/>
-            <Route path={PATH_DASHBOARD.sendMessage} element={<SendMessagePage/>}/>
-            <Route path={PATH_DASHBOARD.inbox} element={<InboxPage/>}/>
-            <Route path={PATH_DASHBOARD.myLogs} element={<MyLogsPage/>}/>
-            <Route path={PATH_DASHBOARD.user} element={<UserPage/>}/>
-        </Route>
-
-        {/*Protected Roles manager*/}
-        <Route element={<AuthGuard roles={managerAccessRoles}/>}>
-            <Route path={PATH_DASHBOARD.dashboard} element={<ManagerPage/>}/>
-        </Route>
-        
-        {/*Protected Roles admin*/}
-        <Route element={<AuthGuard roles={adminAccessRoles}/>}>
-            <Route path={PATH_DASHBOARD.usersManagement} element={<UserManagementPage/>}/>
-            <Route path={PATH_DASHBOARD.updateRole} element={<UpdateRolePage/>}/>
-            <Route path={PATH_DASHBOARD.allMessages} element={<AllMessagesPage/>}/>
-            <Route path={PATH_DASHBOARD.systemLogs} element={<SystemLogsPage/>}/>
-            <Route path={PATH_DASHBOARD.admin} element={<AdminPage/>}/>
-        </Route>
-
-        {/*Protected Roles owner*/}
-        <Route element={<AuthGuard roles={ownerAccessRoles}/>}>
-            <Route path={PATH_DASHBOARD.owner} element={<OwnerPage/>}/>
-        </Route>
-        {/*Not Found */}
-        <Route path={PATH_PUBLIC.notFound} element={<NotFoundPage/>}/>
-        <Route path='*' element={<Navigate to={PATH_PUBLIC.notFound} replace/>}/>
-    </Routes>
-  )
-}
+      </Routes>
+    );
+  };
+  
 export default GlobalRouter
