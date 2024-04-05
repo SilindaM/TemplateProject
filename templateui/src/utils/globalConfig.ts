c2c9c0215f0c48ec5bd4abac5870ef52761b117f
silindaM <silindanemdu@gmail.com>
2024-04-05 22:09:36 +0200

    push all

--- templateui/src/utils/globalConfig.ts
+++ templateui/src/utils/globalConfig.ts
@@ -6,11 +6,11 @@ export const REGISTER_URL = '/Auth/register';
 export const LOGIN_URL = '/Auth/login';
 export const ME_URL = '/Auth/me';
 export const USERS_LIST_URL = '/Auth/users';
-export const UPDATE_ROLE_URL = '/Auth/update-role';
+export const UPDATE_ROLE_URL = '/Auth/UpdateRoles';
 export const USERNAMES_LIST_URL = '/Auth/usernames';
-export const ALL_MESSAGES_URL = '/Messages';
-export const CREATE_MESSAGE_URL = '/Messages/create';
-export const MY_MESSAGE_URL = '/Messages/mine';
+export const ALL_MESSAGES_URL = '/Message';
+export const CREATE_MESSAGE_URL = '/Message/create';
+export const MY_MESSAGE_URL = '/Message/mine';
 export const LOGS_URL = '/Logs';
 export const MY_LOGS_URL = '/Logs/mine';
 
