1e142f9555abbee217266d954fa0fdd817503a55
silindaM <silindanemdu@gmail.com>
2024-04-05 09:48:23 +0200

    basic file to run sql scripts

---
+++ Persistence/sql.sql
@@ -0,0 +1,7 @@
+﻿select * from Users;
+select * from AspNetRoles
+select * from AspNetUserRoles
+
+UPDATE AspNetUserRoles
+SET RoleId = '18a5e690-0cc7-4295-9a64-ca60e345e2f9'
+WHERE UserId='dd34d4cb-e5ae-4306-ad32-29eac20b8fe4';
